1cc68e0ac4b5db11b24b68a16532ed8f8a289ab8
Abraham Murciano <abrahammurciano@gmail.com>
2021-08-18 18:31:12 +0300

    first commit

---
+++ typed_data_structures/__version__.py
@@ -0,0 +1,17 @@
+"""
+__version__.py
+~~~~~~~~~~~~~~
+
+Information about the current version of the typed-data-structures package.
+"""
+
+__title__ = "typed-data-structures"
+__description__ = (
+	"typed-data-structures - A collection of data structures written in python with"
+	" propper type annotations"
+)
+__version__ = "0.0.1"
+__author__ = "Abraham Murciano"
+__author_email__ = "abrahammurciano@gmail.com"
+__license__ = "MIT"
+__url__ = "https://github.com/abrahammurciano/typed-data-structures"
